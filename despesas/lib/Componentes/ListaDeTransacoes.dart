69e6c2ebb6ee1437b5d14c133eae7db99d19e926
Bazch <mikael.vidal@gmail.com>
2024-01-29 14:14:55 -0300

    Lista as transações (ListViewAdicionado)

--- despesas/lib/Componentes/ListaDeTransacoes.dart
+++ despesas/lib/Componentes/ListaDeTransacoes.dart
@@ -3,66 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ListaDeTransacoes extends StatelessWidget {
-  List<Transacao> transacoes;
+  final List<Transacao> transacoes;
 
   ListaDeTransacoes(this.transacoes);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transacoes.map((transacoes) {
-        return Card(
-          color: Colors.blue,
-          child: Row(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                child: Text(
-                  'R\$ ${transacoes.valor.toStringAsFixed(2)}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50.0),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 1,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    child: Text(
-                      transacoes.titulo,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.black,
-                      ),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: transacoes.length,
+        itemBuilder: (ctx,index){
+            final tr = transacoes[index]; 
+            return Card(
+            color: Colors.blue,
+            child: Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  child: Text(
+                    'R\$ ${tr.valor.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
                   ),
-                  Container(
-                    child: Text(
-                      DateFormat('d/MMM/y H:m:s').format(transacoes.data),
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10,
-                        color: Colors.grey,
-                      ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.0),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
-      }).toList(),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        tr.titulo,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        DateFormat('d/MMM/y H:m:s').format(tr.data),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
