f8ed43066d95a0ff76989bc45cb9efaeae3884d9
Bazch <mikael.vidal@gmail.com>
2024-01-29 14:14:33 -0300

    formulario para adicionar novas transações

--- despesas/lib/Componentes/FormularioDeTransacoes.dart
+++ despesas/lib/Componentes/FormularioDeTransacoes.dart
@@ -4,38 +4,52 @@ class FormularioDeTransacoes extends StatelessWidget {
   final controladorTitulo = TextEditingController();
   final controladorValor = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  FormularioDeTransacoes(this.onSubmit);
+
+  _submitForm() {
+    final titulo = controladorTitulo.text;
+    final valor = double.tryParse(controladorValor.text) ?? 0.0;
+    if(titulo.isEmpty || valor <= 0){
+      return;
+    }
+    onSubmit(titulo, valor);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    controller: controladorTitulo,
-                    decoration:InputDecoration(labelText: 'Titulo da transação'),),
-                  TextField(
-                    controller: controladorValor,
-                    decoration: InputDecoration(labelText: 'Valor (R\$)'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      
-                      TextButton(                    
-                        onPressed: () {
-                        print('Titulo da compra: ' + controladorTitulo.text);
-                        print('Valor: '+ controladorValor.text);
-                        },
-                        child: Text('Nova transação',style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold, color: Colors.purple)),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: controladorTitulo,
+              decoration: InputDecoration(labelText: 'Titulo da transação'),
             ),
-          );
+            TextField(
+              controller: controladorValor,
+              decoration: InputDecoration(labelText: 'Valor (R\$)'),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () => _submitForm(),
+                  child: Text('Nova transação',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
