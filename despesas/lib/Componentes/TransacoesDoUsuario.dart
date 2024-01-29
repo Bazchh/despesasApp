3a1fec15de14615693932a435e1dd76aceb59211
Bazch <mikael.vidal@gmail.com>
2024-01-29 14:15:45 -0300

    Engloba dois widgets, lista de transações e o forms

--- despesas/lib/Componentes/TransacoesDoUsuario.dart
+++ despesas/lib/Componentes/TransacoesDoUsuario.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:despesas/Componentes/ListaDeTransacoes.dart';
 import 'package:despesas/Modelos/Transacao.dart';
 import 'package:flutter/material.dart';
@@ -10,30 +12,25 @@ class TransacoesDoUsuario extends StatefulWidget {
 }
 
 class _TransacoesDoUsuarioState extends State<TransacoesDoUsuario> {
-  final _transacoes = [
-    Transacao(
-        id: '1',
-        titulo: 'primeira transacao',
-        valor: 200.00,
-        data: DateTime.now()),
-    Transacao(
-        id: '2',
-        titulo: 'segunda transacao',
-        valor: 245.01,
-        data: DateTime.now()),
-    Transacao(
-        id: '3',
-        titulo: 'terceira transacao',
-        valor: 155.00,
-        data: DateTime.now()),
-  ];
+  final List<Transacao> _transactions = [];
+
+  void _adicionaNovaTransacao(String titulo, double valor) {
+    final novaTransacao = Transacao(
+        id: Random().nextDouble().toString(),
+        titulo: titulo,
+        valor: valor,
+        data: DateTime.now());
+    setState(() {
+      _transactions.add(novaTransacao);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        ListaDeTransacoes(_transacoes),
-        FormularioDeTransacoes()
+        FormularioDeTransacoes(_adicionaNovaTransacao),
+        ListaDeTransacoes(_transactions),
       ],
     );
   }
