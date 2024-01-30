64570074d5f23a2c156c49d753cbcbd57da7c316
Bazch <mikael.vidal@gmail.com>
2024-01-30 10:57:43 -0300

    Criando tema para o app

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,30 +1,113 @@
 // ignore_for_file: sort_child_properties_last
+import 'dart:math';
 
-import 'package:despesas/Componentes/DespesasAppInterface.dart';
-import 'package:despesas/Componentes/TransacoesDoUsuario.dart';
+import 'package:despesas/Componentes/ListaDeTransacoes.dart';
 import 'package:despesas/Modelos/Transacao.dart';
+import 'package:despesas/Componentes/FormularioDeTransacoes.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(despesasApp());
 
 class despesasApp extends StatelessWidget {
+  final ThemeData tema = ThemeData();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: homePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme
+            .copyWith(primary: Colors.green[500], secondary: Colors.green[700]),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
 
-class homePage extends StatelessWidget {
+class homePage extends StatefulWidget {
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  final List<Transacao> _transactions = [];
+
+  void _adicionaNovaTransacao(String titulo, double valor) {
+    final novaTransacao = Transacao(
+        id: Random().nextDouble().toString(),
+        titulo: titulo,
+        valor: valor,
+        data: DateTime.now());
+    setState(() {
+      _transactions.add(novaTransacao);
+    });
+
+    Navigator.of(context).pop();
+  }
+
+  _abrirTransacaoModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return FormularioDeTransacoes(_adicionaNovaTransacao);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DespesasApp'),
-        backgroundColor: Colors.blue,
+        title: Text('DespesasApp',
+        style: Theme.of(context).appBarTheme.titleTextStyle),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actions: <Widget>[
+          IconButton(
+              onPressed: () => _abrirTransacaoModal(context),
+              icon: Icon(Icons.add))
+        ],
       ),
-      body: DespesasAppInterface(),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                // ignore: prefer_const_constructors
+                child: Card(
+                  child: Text(
+                    'Página inicial',
+                  ),
+                  elevation: 5,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              ListaDeTransacoes(_transactions),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _abrirTransacaoModal(context),
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
