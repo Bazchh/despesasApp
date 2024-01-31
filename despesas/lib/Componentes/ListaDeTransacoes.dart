65e7cdfb23b7012f55995e04fe118101d464b48a
Bazch <mikael.vidal@gmail.com>
2024-01-31 16:44:47 -0300

    Adição de imagem para caso de lista vazia

--- despesas/lib/Componentes/ListaDeTransacoes.dart
+++ despesas/lib/Componentes/ListaDeTransacoes.dart
@@ -11,61 +11,82 @@ class ListaDeTransacoes extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 730,
-      child: ListView.builder(
-        itemCount: transacoes.length,
-        itemBuilder: (ctx, index) {
-          final tr = transacoes[index];
-          return Card(
-            child: Row(
-              children: [
+      child: transacoes.isEmpty
+          ? Column(
+              children: <Widget>[
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  margin: EdgeInsets.symmetric(vertical: 10),
                   child: Text(
-                    'R\$ ${tr.valor.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
+                    'Nenhuma transação cadastrada',
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50.0),
-                    border: Border.all(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 1,
-                    ),
-                  ),
-                  padding: EdgeInsets.all(10),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: Text(
-                        tr.titulo,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: Text(
-                        DateFormat('d/MMM/y H:m:s').format(tr.data),
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 10,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ],
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20),
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ],
+            )
+          : ListView.builder(
+              itemCount: transacoes.length,
+              itemBuilder: (ctx, index) {
+                final tr = transacoes[index];
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                        child: Text(
+                          'R\$ ${tr.valor.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).colorScheme.secondary,
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                          border: Border.all(
+                            color: Theme.of(context).colorScheme.secondary,
+                            width: 1,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Text(
+                              tr.titulo,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              DateFormat('d/MMM/y H:m:s').format(tr.data),
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 10,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
